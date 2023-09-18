8d6d1a3cd42531f4ad0f8678eec66dab1b9e45a0
Max Metral <max.metral@sesamecare.com>
2023-09-17 23:12:50 -0400

    fix(otlp): dummy exporter needs to flush records

--- src/telemetry/DummyExporter.ts
+++ src/telemetry/DummyExporter.ts
@@ -1,11 +1,18 @@
-import { SpanExporter } from '@opentelemetry/sdk-trace-base';
+import { ExportResultCode } from '@opentelemetry/core';
+import type { SpanExporter } from '@opentelemetry/sdk-trace-base';
+
+const noop: SpanExporter['export'] = (spans, resultCallback) => {
+  setImmediate(() =>
+    resultCallback({
+      code: ExportResultCode.SUCCESS,
+    }),
+  );
+};
 
 export class DummySpanExporter implements SpanExporter {
-  export() {
-    // Nothing
-  }
+  export = noop;
 
   async shutdown() {
-    // Nothing
+    // Nothing to do
   }
 }
