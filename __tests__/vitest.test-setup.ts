00c49832db170a949af9277603c0d1823f8dc503
Max Metral <max@pyralis.com>
2024-04-28 17:23:31 -0400

    add a comment

--- __tests__/vitest.test-setup.ts
+++ __tests__/vitest.test-setup.ts
@@ -2,6 +2,8 @@ import { afterAll, beforeAll } from 'vitest';
 
 import { shutdownGlobalTelemetry, startGlobalTelemetry } from '../src/telemetry';
 
+// Even in testing, this needs to run first so that the instrumentation
+// is loaded BEFORE express is loaded.
 const startPromise = startGlobalTelemetry('fake-serv');
 
 beforeAll(async () => {
